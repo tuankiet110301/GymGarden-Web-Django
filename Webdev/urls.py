c4090e9ccdc7841e549ba0af1db2ec4217601e92
tuankiet110301 <63094668+tuankiet110301@users.noreply.github.com>
2021-11-27 19:22:22 +0700

    Update urls.py

--- Webdev/urls.py
+++ Webdev/urls.py
@@ -32,5 +32,3 @@ urlpatterns = [
     url(r'^faq/$', faq),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
