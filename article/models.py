07aee67994c0bcea1690de671f95a3cb12ea57e8
tuankiet110301 <63094668+tuankiet110301@users.noreply.github.com>
2021-11-27 21:49:12 +0700

    Update models.py

--- article/models.py
+++ article/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
-    content = models.CharField()
+    content = models.TextField()
     thumbnail_content = models.TextField(max_length=100, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
